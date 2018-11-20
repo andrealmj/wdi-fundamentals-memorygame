4aa3421f74123ec13e3d0ec0105199987f983a80
Andrea <andrealmj@live.com>
2018-11-20 15:00:41 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,23 +24,35 @@ var cards = [
 var cardsInPlay = [0, 2];
 
 var checkForMatch = function () {
+	document.getElementsByTagName('cards')[0].setAttribute('src', cards.cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-}
-
-var flipCard = function (cardId) {
-	checkForMatch ();
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
 };
 
-flipCard(0);
+var flipCard = function (cardId) {
+	var cardId = this.getAttribute('data-id');
+	checkForMatch ();
+	console.log("User flipped " + cards.rank);
+	cardsInPlay.push(cards.rank);
+};
+
 console.log(cards[0].cardImage);
 console.log(cards[0].suit);
 
-flipCard(2);
 console.log(cards[2].cardImage);
 console.log(cards[2].suit);
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
